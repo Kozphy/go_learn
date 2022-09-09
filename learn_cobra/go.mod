cc5183845da2b87cfd71324aafe9bb445eaee149
Zixas <dbdf0369@gmail.com>
2022-09-09 22:37:00 +0800

    update

--- learn_cobra/go.mod
+++ learn_cobra/go.mod
@@ -1,3 +1,22 @@
 module github.com/zixas/learn_cobra
 
 go 1.19
+
+require (
+	github.com/sirupsen/logrus v1.9.0
+	github.com/spf13/cobra v1.5.0
+	github.com/x-cray/logrus-prefixed-formatter v0.5.2
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.20.2 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
+)
--- learn_cobra/pkg/cmd/root.go
+++ learn_cobra/pkg/cmd/root.go
@@ -1,7 +1,47 @@
 package cmd
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	// "github.com/spf13/viper"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
+)
+
+func init() {
+	log.SetFormatter(&prefixed.TextFormatter{})
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "learn_cobra",
+	Short: "cobra learn",
+	Long:  `....`,
+
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		if err := cobraLoadDotenv(cmd, args); err != nil {
+			return nil
+		}
+		return nil
+	},
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+}
+
+func cobraLoadDotenv(cmd *cobra.Command, args []string) error {
+	return nil
+}
 
 func Execute() {
-	fmt.Println("hello")
+	log.Info("A walrus appears")
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
 }
